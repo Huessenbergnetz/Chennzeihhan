78f7fd9c1a1acb1d36d15e7f000eb294c60f3751
buschmann23 <buschmann@huessenbergnetz.de>
2014-02-27 00:45:34 +0100

    translation update

--- l10n/chennzeihhan_de.ts
+++ l10n/chennzeihhan_de.ts
@@ -13,16 +13,21 @@
     </message>
     <message>
         <location filename="../qml/Pages/About.qml" line="66"/>
-        <source>Licensed under the &lt;a href=&apos;http://www.gnu.org/licenses/gpl-2.0.en.html&apos;&gt;GNU GPL v2&lt;/a&gt;</source>
-        <translation>Lizensiert unter der &lt;a href=&apos;http://www.gnu.org/licenses/gpl-2.0.en.html&apos;&gt;GNU GPL v2&lt;/a&gt;</translation>
+        <source>Source code is licensed under the &lt;a href=&apos;http://www.gnu.org/licenses/gpl-2.0.en.html&apos;&gt;GNU GPL v2&lt;/a&gt;</source>
+        <translation>Quellcode ist lizenziert unter der &lt;a href=&apos;http://www.gnu.org/licenses/gpl-2.0.en.html&apos;&gt;GNU GPL v2&lt;/a&gt;</translation>
     </message>
     <message>
-        <location filename="../qml/Pages/About.qml" line="77"/>
+        <location filename="../qml/Pages/About.qml" line="78"/>
+        <source>Car plates database is licensed under &lt;a href=&apos;http://creativecommons.org/licenses/by-nc-sa/4.0/&apos;&gt;Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International&lt;/a&gt;</source>
+        <translation>Kennzeichendatenbank ist lizenziert unter &lt;a href=&apos;http://creativecommons.org/licenses/by-nc-sa/4.0/&apos;&gt;Creative Commons Namensnennung - Nicht-kommerziell - Weitergabe unter gleichen Bedingungen 4.0 International&lt;/a&gt;</translation>
+    </message>
+    <message>
+        <location filename="../qml/Pages/About.qml" line="90"/>
         <source>Visit website</source>
         <translation>Webseite besuchen</translation>
     </message>
     <message>
-        <location filename="../qml/Pages/About.qml" line="84"/>
+        <location filename="../qml/Pages/About.qml" line="97"/>
         <source>Make a donation</source>
         <translation>Spenden</translation>
     </message>
@@ -168,27 +173,9 @@
 <context>
     <name>CountriesDelegate</name>
     <message>
-        <location filename="../qml/Delegates/CountriesDelegate.qml" line="30"/>
-        <source>Not installed</source>
-        <translation>Nicht installiert</translation>
-    </message>
-</context>
-<context>
-    <name>CountriesModel</name>
-    <message>
-        <location filename="../qml/Models/CountriesModel.qml" line="9"/>
-        <source>Austria</source>
-        <translation>Österreich</translation>
-    </message>
-    <message>
-        <location filename="../qml/Models/CountriesModel.qml" line="10"/>
-        <source>Switzerland</source>
-        <translation>Schweiz</translation>
-    </message>
-    <message>
-        <location filename="../qml/Models/CountriesModel.qml" line="11"/>
-        <source>Germany</source>
-        <translation>Deutschland</translation>
+        <location filename="../qml/Delegates/CountriesDelegate.qml" line="83"/>
+        <source>Romving from favourites</source>
+        <translation>Entferne von Favoriten</translation>
     </message>
 </context>
 <context>
@@ -215,32 +202,42 @@
 <context>
     <name>CountryView</name>
     <message>
-        <location filename="../qml/Views/CountryView.qml" line="32"/>
+        <location filename="../qml/Views/CountryView.qml" line="39"/>
         <source>Current search: license plate</source>
         <translation>Aktuelle Suche: Kennzeichen</translation>
     </message>
     <message>
-        <location filename="../qml/Views/CountryView.qml" line="32"/>
+        <location filename="../qml/Views/CountryView.qml" line="39"/>
         <source>Current search: name</source>
         <translation>Aktuelle Suche: Name</translation>
     </message>
     <message>
-        <location filename="../qml/Views/CountryView.qml" line="32"/>
+        <location filename="../qml/Views/CountryView.qml" line="39"/>
         <source>Current search: plate and name</source>
         <translation>Aktuelle Suche: Kennzeichen und Name</translation>
     </message>
     <message>
-        <location filename="../qml/Views/CountryView.qml" line="37"/>
+        <location filename="../qml/Views/CountryView.qml" line="44"/>
         <source>Current order: license plate</source>
         <translation>Aktuelle Sortierung: Kennzeichen</translation>
     </message>
     <message>
-        <location filename="../qml/Views/CountryView.qml" line="37"/>
+        <location filename="../qml/Views/CountryView.qml" line="44"/>
         <source>Current order: by name</source>
         <translation>Aktuelle Sortierung: Name</translation>
     </message>
     <message>
-        <location filename="../qml/Views/CountryView.qml" line="61"/>
+        <location filename="../qml/Views/CountryView.qml" line="59"/>
+        <source>Information to the local breakdown of the vehicle registration plate system in %1 will be part of the premium version.</source>
+        <translation>Informationen zur lokalen Aufschlüsselung des Kennzeichensystems von %1 werden Teil der Premiumversion sein.</translation>
+    </message>
+    <message>
+        <location filename="../qml/Views/CountryView.qml" line="64"/>
+        <source>The vehicle registration plate system of %1 does not use a breakdown into districts, regions or something similar. The premium version will contain further information. </source>
+        <translation>Das Kennzeichensystem von %1 wird nicht weiter in Distrikte, Regionen oder ähnliches aufgeschlüsselt. Die Premiumversion wird weitere Informationen enthalten.</translation>
+    </message>
+    <message>
+        <location filename="../qml/Views/CountryView.qml" line="79"/>
         <source>Search</source>
         <translation>Suche</translation>
     </message>
@@ -384,6 +381,40 @@
     </message>
 </context>
 <context>
+    <name>Help</name>
+    <message>
+        <location filename="../qml/Pages/Help.qml" line="19"/>
+        <location filename="../qml/Pages/Help.qml" line="86"/>
+        <source>Help</source>
+        <translation>Hilfe</translation>
+    </message>
+    <message>
+        <location filename="../qml/Pages/Help.qml" line="21"/>
+        <source>Legend</source>
+        <translation>Legende</translation>
+    </message>
+    <message>
+        <location filename="../qml/Pages/Help.qml" line="40"/>
+        <source>You have marked this as a favourite for faster access.</source>
+        <translation>Sie haben dies zum schnelleren Zugriff als Favorit markiert.</translation>
+    </message>
+    <message>
+        <location filename="../qml/Pages/Help.qml" line="61"/>
+        <source>This is an official code that appears in the UN list of distinguishing codes.</source>
+        <translation>Dies ist ein offizielles Nationalitätskennzeichen, das in der entsprechenden UN-Liste aufgeführt wird.</translation>
+    </message>
+    <message>
+        <location filename="../qml/Pages/Help.qml" line="82"/>
+        <source>This country uses a deeper categorized system of registration plate code signs.</source>
+        <translation>Dieses Land nutzt ein lokal aufgeschlüsseltes System von Kennzeichen.</translation>
+    </message>
+    <message>
+        <location filename="../qml/Pages/Help.qml" line="93"/>
+        <source>To add a country to the list of favourites, press and hold the list entry.</source>
+        <translation>Um ein Land zur Favoritenliste hinzuzufügen, halten Sie den entsprechenden Eintrag gedrückt.</translation>
+    </message>
+</context>
+<context>
     <name>ItemEntry</name>
     <message>
         <location filename="../qml/ItemViews/ItemEntry.qml" line="98"/>
@@ -417,24 +448,61 @@
 <context>
     <name>MainView</name>
     <message>
-        <location filename="../qml/Views/MainView.qml" line="22"/>
+        <location filename="../qml/Views/MainView.qml" line="158"/>
         <source>About</source>
         <translation>Über</translation>
     </message>
     <message>
-        <location filename="../qml/Views/MainView.qml" line="26"/>
+        <location filename="../qml/Views/MainView.qml" line="162"/>
+        <source>Help/Legend</source>
+        <translation>Hilfe/Legende</translation>
+    </message>
+    <message>
+        <location filename="../qml/Views/MainView.qml" line="166"/>
         <source>Settings</source>
         <translation>Einstellungen</translation>
     </message>
     <message>
-        <location filename="../qml/Views/MainView.qml" line="39"/>
+        <location filename="../qml/Views/MainView.qml" line="176"/>
+        <source>Search ...</source>
+        <translation>Suche ...</translation>
+    </message>
+    <message>
+        <location filename="../qml/Views/MainView.qml" line="349"/>
+        <source>Sorting</source>
+        <translation>Sortierung</translation>
+    </message>
+    <message>
+        <location filename="../qml/Views/MainView.qml" line="354"/>
+        <location filename="../qml/Views/MainView.qml" line="369"/>
+        <source>Code</source>
+        <translation>Zeichen</translation>
+    </message>
+    <message>
+        <location filename="../qml/Views/MainView.qml" line="355"/>
+        <location filename="../qml/Views/MainView.qml" line="370"/>
+        <source>Name</source>
+        <translation>Name</translation>
+    </message>
+    <message>
+        <location filename="../qml/Views/MainView.qml" line="364"/>
+        <source>Search</source>
+        <translation>Suche</translation>
+    </message>
+    <message>
+        <location filename="../qml/Views/MainView.qml" line="371"/>
+        <source>Both</source>
+        <translation>Beides</translation>
+    </message>
+    <message>
+        <location filename="../qml/Views/MainView.qml" line="381"/>
         <source>You have no database installed. The database is not part of the application package to allow database updates without releasing new application versions. Go to the settings to download the database.</source>
         <translation>Sie haben noch keine Datenbank installiert. Um die Datenbank unabhängig von der Anwendung aktualisieren zu können ist diese nicht Teil des Installationspakets. Rufen Sie die Einstellungen auf, um die Datenbank herunter zu laden.</translation>
     </message>
     <message>
-        <location filename="../qml/Views/MainView.qml" line="53"/>
-        <source>New content available, please update database</source>
-        <translation>Neue Inhalte verfügbar, bitte die Datenbank aktualisieren.</translation>
+        <location filename="../qml/Views/MainView.qml" line="386"/>
+        <source>Because of changes in the data models you have to update the database. Please go to the settings to do so. We apologize for the inconvenience.</source>
+        <translation>Auf Grund von Änderungen in den Datenmodellen müssen Sie die Datenbank aktualisieren. Rufen Sie dafür bitte die Einstellungen auf.</translation>
     </message>
 </context>
 <context>
@@ -473,6 +541,38 @@
         <location filename="../qml/Pages/Settings.qml" line="70"/>
         <source>Available database revision: </source>
         <translation>Verfügbare Datenbankrevision:</translation>
+    </message>
+    <message>
+        <location filename="../qml/Pages/Settings.qml" line="85"/>
+        <source>Display</source>
+        <translation>Anzeige</translation>
+    </message>
+    <message>
+        <location filename="../qml/Pages/Settings.qml" line="90"/>
+        <source>Default ordering</source>
+        <translation>Standardsortierung</translation>
+    </message>
+    <message>
+        <location filename="../qml/Pages/Settings.qml" line="95"/>
+        <location filename="../qml/Pages/Settings.qml" line="110"/>
+        <source>Code</source>
+        <translation>Zeichen</translation>
+    </message>
+    <message>
+        <location filename="../qml/Pages/Settings.qml" line="96"/>
+        <location filename="../qml/Pages/Settings.qml" line="111"/>
+        <source>Name</source>
+        <translation>Name</translation>
+    </message>
+    <message>
+        <location filename="../qml/Pages/Settings.qml" line="105"/>
+        <source>Default search</source>
+        <translation>Standardsuche</translation>
+    </message>
+    <message>
+        <location filename="../qml/Pages/Settings.qml" line="112"/>
+        <source>Both</source>
+        <translation>Beides</translation>
     </message>
 </context>
 <context>
