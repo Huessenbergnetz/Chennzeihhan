c26e0f4e09f0f9a7702e1e78677ffe174b9ddf12
Buschmann <kontakt@buschmann23.de>
2016-10-10 12:51:55 +0200

    Cleanups

--- src/models/countrymodel.cpp
+++ src/models/countrymodel.cpp
@@ -24,69 +24,11 @@
 #include <QtDebug>
 #endif
 
-//const char* CountryModel::COLUMN_NAMES[] = {
-//    "itemId",
-//    "sign",
-//    "name",
-//    "type",
-//    "state",
-//    "closed",
-//    NULL
-//};
-
 
 CountryModel::CountryModel(QObject *parent) :
     SimpleItemModel(parent)
 {
 }
-
-
-//QHash<int,QByteArray> CountryModel::roleNames() const
-//{
-//    int idx = 0;
-//    QHash<int, QByteArray> roles;
-//    while (COLUMN_NAMES[idx]) {
-//        roles[Qt::UserRole + idx + 1] = COLUMN_NAMES[idx];
-//        idx++;
-//    }
-//    return roles;
-//}
-
-
-
-//QVariant CountryModel::data(const QModelIndex &index, int role) const
-//{
-//    QVariant value = QSqlQueryModel::data(index, role);
-
-//    if(role < Qt::UserRole)
-//    {
-//        value = QSqlQueryModel::data(index, role);
-//    } else {
-//        int columnIdx = role - Qt::UserRole - 1;
-//        QModelIndex modelIndex = this->index(index.row(), columnIdx);
-//        value = QSqlQueryModel::data(modelIndex, Qt::DisplayRole);
-//        if (columnIdx == 3) {
-
-//            if (countryCode == QLatin1String("de")) {
-//                value = deHelper->getType(value.toInt());
-//            } else if (countryCode == QLatin1String("at")) {
-//                value = atHelper->getType(value.toInt());
-//            }
-
-//        } else if (columnIdx == 4) {
-
-//            if (countryCode == QLatin1String("de")) {
-//                value = deHelper->getState(value.toInt());
-//            } else if (countryCode == QLatin1String("at")) {
-//                value = atHelper->getState(value.toInt());
-//            }
-
-//        }
-//    }
-
-//    return value;
-
-//}
 
 
 
@@ -153,48 +95,3 @@ void CountryModel::init()
     setInOperation(false);
 }
 
-
-//void CountryModel::refresh(const QString &cc, int sort, int searchTarget, const QString &search)
-//{
-//    countryCode = cc;
-
-//    QString queryString;
-
-//    if (cc == QLatin1String("ch")) {
-//        queryString = QStringLiteral("SELECT id AS itemId, sign, name, '%1' AS type, '' AS state, '' AS closed FROM ch").arg(tr("Canton"));
-//    } else {
-//        queryString = QStringLiteral("SELECT id AS itemId, sign, name, type, state, closed FROM %1").arg(cc);
-//    }
-
-//    if (!search.isEmpty())
-//    {
-//        QString t_search = search;
-//        t_search.append("%");
-
-//        switch(searchTarget)
-//        {
-//        case 1:
-//            queryString.append(QStringLiteral(" WHERE name LIKE \"%1\"").arg(t_search));
-//            break;
-//        case 2:
-//            queryString.append(QStringLiteral(" WHERE (name LIKE \"%1\") OR (sign LIKE \"%2\")").arg(t_search, t_search.toUpper()));
-//            break;
-//        default:
-//            queryString.append(QStringLiteral(" WHERE sign LIKE \"%1\"").arg(t_search.toUpper()));
-//            break;
-//        }
-//    }
-
-//    switch(sort)
-//    {
-//    case 1:
-//        queryString.append(" ORDER BY name ASC");
-//        break;
-//    default:
-//        queryString.append(" ORDER BY sign ASC");
-//        break;
-//    }
-
-
-//    this->setQuery(queryString);
-//}
--- src/models/countrymodel.h
+++ src/models/countrymodel.h
@@ -1,20 +1,13 @@
 #ifndef COUNTRYMODEL_H
 #define COUNTRYMODEL_H
 
-//#include <QSqlQueryModel>
 #include "simpleitemmodel.h"
-
-//#include "athelper.h"
-//#include "dehelper.h"
 
 class CountryModel : public SimpleItemModel
 {
     Q_OBJECT
 public:
     explicit CountryModel(QObject *parent = nullptr);
-
-//    QVariant data(const QModelIndex &index, int role) const;
-//    QHash<int,QByteArray> roleNames() const;
 
     QString countryCode() const;
     bool inOperation() const;
@@ -24,14 +17,7 @@ public:
 signals:
     void inOperationChanged(bool inOperation);
 
-public slots:
-//    void refresh(const QString &cc, int sort, int searchTarget, const QString &search);
-
 private:
-//    const static char* COLUMN_NAMES[];
-//    AtHelper *atHelper;
-//    DeHelper *deHelper;
-//    QString countryCode;
     Q_DISABLE_COPY(CountryModel)
     QString m_countryCode;
     bool m_inOperation;
